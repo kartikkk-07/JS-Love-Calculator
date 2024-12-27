335e89fb6eb967dcdf8a699764c86a14e9ad858c
Kartik Magdum <119559799+kartikkk-07@users.noreply.github.com>
2024-12-27 10:34:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calculateLove(){
     const name2 = document.getElementById("name2").value.trim();
     
     if(name1==="" || name2 ===""){  
-        alert("Please enter both names.");
+        alert("Please enter name in both name-fields.");
     } else{
         const lovePercentage = Math.floor(Math.random() * 50) + 50;
         const result = document.getElementById("result");
